ea8b1fb4226acd7784eb7e8e442f96f8513ed74e
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-23 05:32:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,23 @@
+#made class and named Artist
 class Artist 
-  
+  #getters and setters for variables to use on methods on the class
   attr_accessor :name
-  
+  #counter is set at 0 
   @@song_count = 0
-  
+  #initialize instance variable and an empty array 
   def initialize(name)
     @name = name
     @songs = []
+  end
+  #has many songs 
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
   
     def add_song(song)
     @songs << song
     song.artist = self
     @@song_count +=1 
-  end
-  
- def songs
-  # @songs
-  #end
-  Song.all.select {|song| song.artist == self}
   end
   
   def add_song_by_name(name)
